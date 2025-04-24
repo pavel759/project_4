c2b44084b632718ec9c32ce6e1c38bb13a8e7372
Pavel Dev <segodnagromgremit@mail.ru>
2025-04-24 13:02:04 +0300

    finish html

---
+++ script/main.js
@@ -0,0 +1,25 @@
+let swiperCard = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 20,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        577: {
+            slidesPerView: 2
+        },
+        769: {
+            slidesPerView: 3
+        }
+    }
+
+});
